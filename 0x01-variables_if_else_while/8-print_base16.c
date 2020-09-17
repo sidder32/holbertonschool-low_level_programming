bf9cabee431fa3add73a4c983e321194a7bd7c9a
sidder <2526@holbertonschool.com>
2020-09-17 22:59:11 +0200

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,10 +10,11 @@
 int main(void)
 {
 int c;
-for(c = '0'; c<= '9'; c++){
+for (c = '0'; c <= '9'; c++)
+{
 putchar(c);
 }
-for(c = 'a'; c<= 'f'; c++)
+for (c = 'a'; c <= 'f'; c++)
 putchar(c);
 putchar('\n');
 return (0);
