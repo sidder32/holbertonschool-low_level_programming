65439ce3229759cbdeee0f29919496cc508dec00
sidder <2526@holbertonschool.com>
2020-09-18 18:32:02 +0200

    task 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int j;
+int c;
+
+for (i = '0' ; i <= '7' ; ++i)
+{
+for (j = i + 1 ; j <= '8' ; ++j)
+{
+for (c = i + 2 ; c <= '9' ; ++c)
+{
+if (i != j && j != c)
+{
+putchar(i);
+putchar(j);
+putchar(c);
+
+if (i == '7' && j == '8' && c == '9')
+putchar('\n');
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+return (0);
+}
