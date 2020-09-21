4e53a71a0fd6cea7388e7190d705744d2979fb27
sidder <2526@holbertonschool.com>
2020-09-21 23:43:14 +0200

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c : character to check
+ *
+ * Return: 1 if character is letter lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
