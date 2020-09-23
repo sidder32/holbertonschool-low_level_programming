71fc234f8ed38c6f35f4e8de50cb7550d5698060
sidder <2526@holbertonschool.com>
2020-09-24 00:38:34 +0200

    task12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_times_table - prints time table
+ * @n: param n
+ *
+ * Return: ,
+ */
+void print_times_table(int n)
+{
+int i;
+int j;
+int r;
+if (n <= 15 && n >= 0)
+{
+for (i = 0; i <= n; ++i)
+{
+for (j = 0; j <= n; ++j)
+{
+r = i * j;
+if (j == 0)
+{
+printf("%d", r);
+}
+else
+{
+if (r >= 100)
+printf(" %d", r);
+else if (r >= 10)
+printf("  %d", r);
+else
+printf("   %d", r);
+}
+if (j < n)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
+}
+
+}
