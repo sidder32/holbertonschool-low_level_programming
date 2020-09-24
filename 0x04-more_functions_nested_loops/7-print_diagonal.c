1a3c5b07ccf98387daf9f1fcb0a381838a61435a
sidder <2526@holbertonschool.com>
2020-09-24 23:56:27 +0200

    task7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagonal - draw diagonal line in terminal
+ * @n: param n
+ *
+ * Return: ,
+ */
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n >= 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+
+}
+
+
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,9 +3,10 @@
 
 #include <stdio.h>
 int _putchar(char c);
-#endif /* HOLBERTON_H */
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_line(int n);
+void print_diagonal(int n);
+#endif /* HOLBERTON_H */
