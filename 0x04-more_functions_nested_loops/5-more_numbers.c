35101aae948d5100e758130a318da1e5ca276347
sidder <2526@holbertonschool.com>
2020-09-24 19:38:30 +0200

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,15 +15,11 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j < 10)
-{
-_putchar(j + '0');
-}
-else
+if (j >= 10)
 {
 _putchar(j / 10 + '0');
-_putchar(j % 10 + '0');
 }
+_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
