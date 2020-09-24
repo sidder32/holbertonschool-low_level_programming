9df206cb6611ab8f893c720238e9ef8d40cad374
sidder <2526@holbertonschool.com>
2020-09-24 19:31:41 +0200

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ *
+ *
+ * Return: ,
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j < 10)
+{
+_putchar(j + '0');
+}
+else
+{
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+}
+_putchar('\n');
+}
+
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -3,8 +3,9 @@
 
 #include <stdio.h>
 int _putchar(char c);
-#endif /* HOLBERTON_H */
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
+#endif /* HOLBERTON_H */
